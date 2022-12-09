dd8637ebbc11f8803c21ac89539d4384a44aeaa0
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2022-12-09 10:14:56 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { CurrencyService } from './service/currency.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  selectedCurrency : string = "LKR";
+  constructor(private currencyService : CurrencyService){
+
+  }
+  sendCurrency(event:string){
+
+    this.currencyService.setCurrency(event);
+  }
+}
